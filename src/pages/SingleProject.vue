604b600f4b471a572e29f3f8c0a2adc5a0254f20
DaviGel <geloso.davide@gmail.com>
2024-02-19 15:34:06 +0100

    feat: codice migliorato

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -6,7 +6,8 @@ export default {
 
     data() {
         return {
-            item: null,
+            item: [],
+            loading: false,
             baseUrl: "http://127.0.0.1:8000",
             apiUrl: "/api/projects/",
         };
@@ -14,22 +15,27 @@ export default {
 
     methods: {
         getData() {
+            this.loading = true;
             return axios
                 .get(this.baseUrl + this.apiUrl + this.$route.params.slug)
                 .then((response) => {
-                    if (response.data.success) {
+                    if (response.data.result) {
                         this.item = response.data.result;
                     } else {
+                        this.loading = false;
                         this.$router.push({ name: "not-found" });
                     }
                 })
                 .catch((error) => {
                     console.error("Errore:", error);
+                })
+                .finally(() => {
+                    this.loading = false;
                 });
         },
     },
 
-    mounted() {
+    created() {
         this.getData();
         this.$watch(
             () => this.$route.params,
@@ -42,13 +48,11 @@ export default {
 </script>
 
 <template>
-    <div v-if="!item" class="container py-4">
+    <div v-if="loading" class="container py-4">
         <div>Loading...</div>
     </div>
     <div v-else class="container py-4">
-        <div class="row">
-            <h1 class="">{{ item.title }}</h1>
-            <p>{{ item.description }}</p>
-        </div>
+        <h1>{{ item.title }}</h1>
+        <p>{{ item.description }}</p>
     </div>
 </template>
